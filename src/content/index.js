0e3bc55f59dbc2449a0be7dcd35ee27072bcef42
Jesse Pollak <jesse@pollak.io>
2016-03-05 10:46:56 -0800

    fix #19

--- src/content/index.js
+++ src/content/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 import * as Fields from 'fields'
 
-$(document).on('focus', Fields.SELECTOR, (e) => {
+$(document).on('keydown', Fields.SELECTOR, (e) => {
   Fields.setup($(e.target))
 })
