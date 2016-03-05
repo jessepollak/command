3a778aab026d7b93c8cef18b2d7b546661750ed5
Jesse Pollak <jesse@pollak.io>
2016-03-05 10:55:10 -0800

    initialize on keydown and focus

--- src/content/index.js
+++ src/content/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 import * as Fields from 'fields'
 
-$(document).on('keydown', Fields.SELECTOR, (e) => {
+$(document).on('keypress focus', Fields.SELECTOR, (e) => {
   Fields.setup($(e.target))
 })
