977377d36a342aa06e8467769eda9e8deea4362d
Jesse Pollak <jesse@pollak.io>
2016-04-02 13:39:40 -0700

    docs(help): adds better wiki page for /help command
    Closes #28

--- src/content/commands/Help/Help.js
+++ src/content/commands/Help/Help.js
@@ -2,7 +2,7 @@ import { Redirect } from 'content/types'
 
 export let mount = (field, onDone) => {
   onDone(new Redirect({
-    url: 'https://github.com/jessepollak/command',
+    url: 'https://github.com/jessepollak/command/wiki/Getting-started',
     target: '_blank'
   }))
 }
