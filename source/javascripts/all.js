05c9c975726765b25d8a5ea3a68dfb40c507d716
Jesse Pollak <jesse@pollak.io>
2016-04-04 21:05:28 -0700

    feat(browser-support): adds browser support notification

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+// Check if browser supports
+var isSupported = !!(window.chrome && !navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile/i))
+
+if (!isSupported) {
+  var body = document.querySelector('body')
+  body.className += " unsupported"
+}
