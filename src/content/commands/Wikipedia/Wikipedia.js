079ee15905e6396bba5af3ddab74a27eddac5796
Christopher Hannah <christopher.hannah@me.com>
2016-04-08 10:24:02 +0100

    Basic Wikipedia Searching
    Here is a basic level of functionality for searching Wikipedia.
    
    The results are limited to 5 at the minute, and the results layout
    needs changing.

---
+++ src/content/commands/Wikipedia/Wikipedia.js
@@ -0,0 +1,79 @@
+import _ from 'lodash'
+import $ from 'jquery'
+import React from 'react'
+import classnames from 'classnames'
+import { mountReactComponent } from 'content/commands/mount'
+
+import styles from './Wikipedia.scss'
+import * as Types from 'content/types'
+import * as Search from 'content/components/Search'
+import Container from 'content/components/Container'
+
+const API_BASE = 'https://wikipedia.org/w/api.php'
+
+var items = []
+
+var resultCount = 0
+
+let search = (query, options={}) => {
+
+  return $.ajax({
+    url: API_BASE,
+    data: {
+      action: 'opensearch',
+      search: query,
+      format: 'json',
+      limit: '5',
+      offset: options.offset
+    }
+  }).then((data) => {
+    console.log("Unconverted Data:")
+    console.log(data)
+    for (var i = 0; i < data[1].length; i++) {
+      var item = {id: i, title: data[1][i], excerpt: data[2][i], url: data[3][i]}
+      items[i] = item
+    }
+
+    console.log("Converted Items:")
+    console.log(items)
+    return items
+  })
+}
+
+let WikipediaResult = (props) => {
+  return (
+    <div className={styles.result}>
+        {props.title}
+    </div>
+  )
+}
+
+class Wikipedia extends React.Component {
+  onSelect(result) {
+    this.props.onDone(new Types.Link({
+      href: result.url
+    }))
+  }
+
+  render() {
+    return (
+      <Container {...this.props}>
+        <Search.Widget
+          placeholder="Search Wikipedia..."
+          onSearch={search}
+          onSelect={this.onSelect.bind(this)}
+          onEsc={this.props.onDone}
+          ResultClass={WikipediaResult}
+          columns={1}
+        />
+      </Container>
+    )
+  }
+}
+Wikipedia.propTypes = {
+  onDone: React.PropTypes.func.isRequired
+}
+
+export let match = "wiki"
+export let icon = require("./Wikipedia.png")
+export let mount = mountReactComponent.bind(null, Wikipedia)
